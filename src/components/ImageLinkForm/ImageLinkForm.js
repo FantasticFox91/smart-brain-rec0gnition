e627f7916d6d23dacb8e0ead43ab99f7a75568e9
Grygorii Lysytsia <grygorii_lysytsia@hotmail.comasd>
2021-06-06 23:55:20 +0430

    Debuging

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input type="URL" value ={value}className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <input type="text" value ={ value } className='f4 pa2 w-70 center' onChange={onInputChange}/>
           <button disable={!value} 
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
